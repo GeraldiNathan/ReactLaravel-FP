b4fe58ccb1c1dc5ddf65db2e1e1b18e1f7bb0172
GeraldiNathan <geraldi.tommysaputra03@gmail.com>
2023-12-23 21:41:59 +0800

    feat(Home) : Create component for page home

---
+++ src/components/HomeComponents.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HomeComponent() {
+  return (
+    <div>
+      <p className="flex justify-center mt-2 text-2xl">Ini adalah Home</p>
+    </div>
+  );
+}
+
+export default HomeComponent;

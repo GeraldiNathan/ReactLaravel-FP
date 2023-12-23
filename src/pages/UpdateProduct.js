1f1397521dc09ff8a3197a6cc265105eb039a527
GeraldiNathan <geraldi.tommysaputra03@gmail.com>
2023-12-23 22:29:52 +0800

    feat(Update Product) : Create new page

---
+++ src/pages/UpdateProduct.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/NavbarComponents";
+
+function UpdateProduct() {
+  return (
+    <>
+      <Navbar />
+      <div>Update Page</div>
+    </>
+  );
+}
+
+export default UpdateProduct;

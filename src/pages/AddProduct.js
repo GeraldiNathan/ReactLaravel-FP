1573ab9f20dfd4400bb724003280ee42ba8eca35
GeraldiNathan <geraldi.tommysaputra03@gmail.com>
2023-12-23 22:24:12 +0800

    feat (AddProduct) : Create Page

---
+++ src/pages/AddProduct.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../components/NavbarComponents";
+
+function AddProduct() {
+  return (
+    <>
+      <Navbar />
+
+      <div>
+        <span>Ini adalah halaman add Product</span>
+      </div>
+    </>
+  );
+}
+
+export default AddProduct;

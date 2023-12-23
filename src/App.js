aad9b89dde79ca18d521eacfd46243da84efa5a0
GeraldiNathan <geraldi.tommysaputra03@gmail.com>
2023-12-23 11:57:34 +0800

    fix(app) : Replacement Code

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path="/home" element={<Home />} />
         <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/register" element={<Register />} />
       </Routes>
     </div>

997f617ce15c1eff12787c713314ba4f6fd955e4
GeraldiNathan <geraldi.tommysaputra03@gmail.com>
2023-12-23 21:35:02 +0800

    feat(Home) : Create Navbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,9 +2,43 @@ import React from "react";
 
 function Home() {
   return (
-    <div>
-      <h1>Ini adalah home</h1>
-    </div>
+    <nav className="flex flex-col sm:flex-row sm:text-left sm:justify-between p-4 bg-white shadow sm:items-baseline w-full">
+      <div className="mb-2 sm:mb-0">
+        <a
+          href="#"
+          className="text-2xl no-underline text-gray-600 hover:text-teal-500 font-bold"
+        >
+          Home
+        </a>
+      </div>
+
+      <div className="flex gap-4 justify-center items-center sm:mx-auto">
+        <a
+          href="#"
+          className="text-xl no-underline text-gray-600 hover:text-teal-500 font-bold"
+        >
+          Dashboard
+        </a>
+        <a
+          href="#"
+          className="text-xl no-underline text-gray-600 hover:text-teal-500 font-bold"
+        >
+          Register
+        </a>
+        <a
+          href="#"
+          className="text-xl no-underline text-gray-600 hover:text-teal-500 font-bold"
+        >
+          Login
+        </a>
+      </div>
+
+      {/* Profile */}
+      <div className="font-bold text-xl"> Halo, Geraldi</div>
+      {/* Profile */}
+    </nav>
+
+    
   );
 }
 

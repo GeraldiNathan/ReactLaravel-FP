7e097c328f84aef46a90a5230eadfc87413f1a2e
GeraldiNathan <geraldi.tommysaputra03@gmail.com>
2023-12-23 21:41:37 +0800

    feat(Home) : add jsx property <></>

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,44 +1,47 @@
 import React from "react";
+import HomeComponent from "../components/HomeComponents";
 
 function Home() {
   return (
-    <nav className="flex flex-col sm:flex-row sm:text-left sm:justify-between p-4 bg-white shadow sm:items-baseline w-full">
-      <div className="mb-2 sm:mb-0">
-        <a
-          href="#"
-          className="text-2xl no-underline text-gray-600 hover:text-teal-500 font-bold"
-        >
-          Home
-        </a>
-      </div>
+    <>
+      <nav className="flex flex-col sm:flex-row sm:text-left sm:justify-between p-4 bg-white shadow sm:items-baseline w-full shadow-xl">
+        <div className="mb-2 sm:mb-0">
+          <a
+            href="#"
+            className="text-2xl no-underline text-gray-600 hover:text-teal-500 font-bold"
+          >
+            Home
+          </a>
+        </div>
 
-      <div className="flex gap-4 justify-center items-center sm:mx-auto">
-        <a
-          href="#"
-          className="text-xl no-underline text-gray-600 hover:text-teal-500 font-bold"
-        >
-          Dashboard
-        </a>
-        <a
-          href="#"
-          className="text-xl no-underline text-gray-600 hover:text-teal-500 font-bold"
-        >
-          Register
-        </a>
-        <a
-          href="#"
-          className="text-xl no-underline text-gray-600 hover:text-teal-500 font-bold"
-        >
-          Login
-        </a>
-      </div>
+        <div className="flex gap-6 justify-center items-center sm:mx-auto">
+          <a
+            href="#"
+            className="text-xl no-underline text-gray-600 hover:text-teal-500 font-bold"
+          >
+            Dashboard
+          </a>
+          <a
+            href="#"
+            className="text-xl no-underline text-gray-600 hover:text-teal-500 font-bold"
+          >
+            Register
+          </a>
+          <a
+            href="#"
+            className="text-xl no-underline text-gray-600 hover:text-teal-500 font-bold"
+          >
+            Login
+          </a>
+        </div>
 
-      {/* Profile */}
-      <div className="font-bold text-xl"> Halo, Geraldi</div>
-      {/* Profile */}
-    </nav>
+        {/* Profile */}
+        <div className="font-bold text-xl"> Halo, Geraldi</div>
+        {/* Profile */}
+      </nav>
 
-    
+      <HomeComponent />
+    </>
   );
 }
 

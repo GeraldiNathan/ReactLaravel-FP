6206f7bd479e70d504ca4922eb6569dc3725bad9
GeraldiNathan <geraldi.tommysaputra03@gmail.com>
2023-12-24 16:33:05 +0800

    feat(app) : Add protected view path

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Register from "./pages/Register";
 import Home from "./pages/Home";
 import AddProduct from "./pages/AddProduct";
 import UpdateProduct from "./pages/UpdateProduct";
+import Protected from "./Protected";
 
 function App() {
   return (
@@ -12,8 +13,13 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/addproduct" element={<AddProduct />} />
-        <Route path="/updateproduct" element={<UpdateProduct />} />
+        {/* <Route path="/addproduct" element={<AddProduct />} /> */}
+        {/* <Route path="/updateproduct" element={<UpdateProduct />} /> */}
+        <Route path="/addproduct" element={<Protected Cmp={AddProduct} />} />
+        <Route
+          path="/updateproduct"
+          element={<Protected Cmp={UpdateProduct} />}
+        />
       </Routes>
     </div>
   );

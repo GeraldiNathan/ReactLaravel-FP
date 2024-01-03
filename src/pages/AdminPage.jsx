f1a8e50f0db115377e1b9752347ddf68ef52c3cb
GeraldiNathan <geraldi.tommysaputra03@gmail.com>
2024-01-03 20:23:53 +0800

    feat(admin) : Create admin page

---
+++ src/pages/AdminPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/NavbarComponents";
+import AdminPage from "../components/AdminComponent";
+
+function UpdateProduct() {
+  return (
+    <>
+      <Navbar />
+      <AdminPage />
+    </>
+  );
+}
+
+export default UpdateProduct;

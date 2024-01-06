430b264fcf858083f7b91c8da5445eb273b7cdd3
GeraldiNathan <geraldi.tommysaputra03@gmail.com>
2024-01-07 01:01:34 +0800

    WIP (Admin) : Styling flash message

--- src/components/AdminComponent.jsx
+++ src/components/AdminComponent.jsx
@@ -44,7 +44,7 @@ function AdminPage() {
 
       setTimeout(() => {
         setFlashMessage(null);
-      }, 780); //780 refers to second or how long the message will pop up
+      }, 3000); //3000 refers to second or how long the message will pop up
     }
   }
 
@@ -71,13 +71,44 @@ function AdminPage() {
       {flashMessage && (
         <div
           className={`fixed left-1/2 transform -translate-x-1/2 z-50 p-4 mb-4 rounded-md ${
-            flashMessage.type === "success" ? "bg-red-500" : "bg-green-500"
+            flashMessage.type === "success"
+              ? "bg-red-100 border border-red-400 text-red-700"
+              : "bg-red-100 border border-red-400 text-red-700"
           }`}
+          role="alert"
         >
-          {flashMessage.message}
+          <div className="flex items-center justify-between">
+            {" "}
+            {/* Container for message and icon "X" */}
+            <div className="flex items-center">
+              {" "}
+              {/* Container message */}
+              <strong class="font-bold">
+                {flashMessage.type === "success" ? "Success! " : "Error!"}
+              </strong>
+              <span class="block sm:inline ml-2">{flashMessage.message}</span>
+            </div>
+            <span className="ml-4" onClick={() => setFlashMessage(null)}>
+              {" "}
+              <svg
+                className={`fill-current h-6 w-6 ${
+                  flashMessage.type === "success"
+                    ? "text-red-500"
+                    : "text-red-500"
+                }`}
+                role="button"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+              >
+                <title>Close</title>
+                <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+              </svg>
+            </span>
+          </div>
         </div>
       )}
       {/* Flash Message */}
+
       <div class="-m-1.5 overflow-x-auto">
         <div class="p-1.5 min-w-full inline-block align-middle">
           <div class="overflow-hidden">

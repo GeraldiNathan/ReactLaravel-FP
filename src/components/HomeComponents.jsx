14c80240f5650f64f6515ab734378ddf837394de
alviyalaela <numbernin9thproject@gmail.com>
2024-01-11 19:09:49 +0700

    Updating dashboard, login and about us page

--- src/components/HomeComponents.jsx
+++ src/components/HomeComponents.jsx
@@ -15,8 +15,8 @@ function HomeComponent() {
 
       <section className="bg-hijau min-h-screen flex items-center">
         <div className="container mx-auto px-4">
-          <div className="text-4xl font-title mt-6 text-white text-center">
-            MEET OUR BEST RECIPE
+          <div className="text-4xl font-bold mt-6 text-kuning text-center">
+            MEET OUR <span className="text-orange">BEST</span> RECIPE
           </div>
           <div className="flex flex-wrap">
             <div className="lg:pt-6 pt-6 w-full md:w-4/12 px-4 text-center">
@@ -99,7 +99,7 @@ function HomeComponent() {
           </div>
         </div>
       </section>
-      <section className="bg-orange min-h-screen items-center">
+      <section className="bg-kuning min-h-screen items-center">
         <div className="container mx-auto px-4 pt-12">
           <div className="max-w-2xl bg-cream flex rounded-2xl shadow-xl mr-auto ml-auto mb-3">
             <div className="w-full md:w-5/12 px-4 mr-auto ml-0">
@@ -121,14 +121,15 @@ function HomeComponent() {
               <button
                 type="submit"
                 className="bg-transparent border-orange border-2 text-orange hover:bg-hijau-500 hover:border-hijau hover:text-hijau font-medium rounded-full py-2 px-4 transition-all duration-300"
+                
               >
                 View More
               </button>
             </div>
           </div>
         </div>
-        <h3 className="text-3xl mt-8 font-bold leading-normal text-cream text-center">
-          Why Choose Our <span className="text-hijau">Food?</span>
+        <h3 className="text-3xl mt-8 font-bold leading-normal text-hijau text-center">
+          Why Choose Our <span className="text-orange">Food?</span>
         </h3>
         <div className="flex flex-wrap ">
           <div className="lg:pt-6 pt-6 w-full md:w-4/12 px-4 text-center">
@@ -142,8 +143,8 @@ function HomeComponent() {
                   viewBox="0 0 24 24"
                   stroke="currentColor"
                 />
-                <h6 className="text-xl font-semibold">Healthier</h6>
-                <p className="mt-2 mb-4 text-gray-600">
+                <h6 className="text-hijau text-xl font-semibold">Healthier</h6>
+                <p className="text-orange mt-2 mb-4">
                 Nourish your body with thoughtfully crafted dishes that prioritize wholesome ingredients, fostering a healthier and happier you. Savor the flavors of nutrition without compromising on taste, making every bite a step towards a vibrant and balanced lifestyle.
                 </p>
               </div>
@@ -161,8 +162,8 @@ function HomeComponent() {
                   viewBox="0 0 24 24"
                   stroke="currentColor"
                 />
-                <h6 className="text-xl font-semibold">Super Taste</h6>
-                <p className="mt-2 mb-4 text-gray-600">
+                <h6 className="text-hijau text-xl font-semibold">Super Taste</h6>
+                <p className="text-orange mt-2 mb-4">
                 Elevate your taste buds to new heights as you indulge in culinary creations that redefine deliciousness. Each dish is a culinary masterpiece, carefully curated to deliver an extraordinary dining experience. Get ready to savor the extraordinary – where every bite is a celebration of superlative taste and culinary artistry.
                 </p>
               </div>
@@ -180,8 +181,8 @@ function HomeComponent() {
                   viewBox="0 0 24 24"
                   stroke="currentColor"
                 />
-                <h6 className="text-xl font-semibold">Best Recipes</h6>
-                <p className="mt-2 mb-4 text-gray-600">
+                <h6 className="text-hijau text-xl font-semibold">Best Recipes</h6>
+                <p className="text-orange mt-2 mb-4">
                 From timeless classics to innovative creations, each recipe stands as a testament to the pursuit of culinary perfection. Explore a world of gastronomic delight where the 'best' is not just a measure but a commitment to excellence in every flavorful bite.
                 </p>
               </div>

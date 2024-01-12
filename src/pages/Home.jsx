72e66ca699fb1c6767859e43e3f85b82993af44f
GeraldiNathan <geraldi.tommysaputra03@gmail.com>
2024-01-12 22:13:11 +0800

    feat(HomePage) : add footerComponent

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import HomeComponent from "../components/HomeComponents";
 import Navbar from "../components/NavbarComponents";
-
+import FooterComponent from "../components/FooterComponent";
 
 function Home() {
   return (
     <>
       <Navbar />
-     
+
       <HomeComponent />
+      <FooterComponent />
     </>
   );
 }

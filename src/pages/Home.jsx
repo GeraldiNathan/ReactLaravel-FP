26be9a277f814847edd932a35b6c87232c1ff271
GeraldiNathan <geraldi.tommysaputra03@gmail.com>
2024-01-13 01:43:05 +0800

    feat(Home Page) : add new component for home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,14 @@ import HomeComponent from "../components/HomeComponents";
 import Navbar from "../components/NavbarComponents";
 import FooterComponent from "../components/FooterComponent";
 import AboutUsComponent from "../components/AboutUsComponent";
+import BestRecipeComponent from "../components/BestRecipeComponent";
 
 function Home() {
   return (
     <>
       <Navbar />
       <HomeComponent />
+      <BestRecipeComponent />
       <AboutUsComponent />
       <FooterComponent />
     </>

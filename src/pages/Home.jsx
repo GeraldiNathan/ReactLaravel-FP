141c4f3c886ac61abe01e97e1374a598705cbe72
GeraldiNathan <geraldi.tommysaputra03@gmail.com>
2024-01-12 22:24:44 +0800

    feat(HomePage) : add about us component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import HomeComponent from "../components/HomeComponents";
 import Navbar from "../components/NavbarComponents";
 import FooterComponent from "../components/FooterComponent";
+import AboutUsComponent from "../components/AboutUsComponent";
 
 function Home() {
   return (
     <>
       <Navbar />
-
       <HomeComponent />
+      <AboutUsComponent />
       <FooterComponent />
     </>
   );

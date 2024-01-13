e92190ab338fd6808483dfa28c6e0a1c586ec937
GeraldiNathan <geraldi.tommysaputra03@gmail.com>
2024-01-14 07:53:35 +0800

    feat(Review Page) : remove unused element

--- src/pages/ReviewPage.jsx
+++ src/pages/ReviewPage.jsx
@@ -1,17 +1,12 @@
 import React from "react";
-import HomeComponent from "../components/HomeComponents";
 import Navbar from "../components/NavbarComponents";
 import RecipeList from "../components/ReviewComponents";
 
 function Review() {
   return (
     <>
-      <div className="bg-backgroundcolor">
-        <Navbar />
-        <div className="mx-72 mt-14 ps-4 pt-3 pb-3  rounded-lg">
-          <RecipeList />
-        </div>
-      </div>
+      <Navbar />
+      <RecipeList />
     </>
   );
 }

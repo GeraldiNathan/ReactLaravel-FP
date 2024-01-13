eff3619b6244645ce869eb94015d1fe165d0112d
GeraldiNathan <geraldi.tommysaputra03@gmail.com>
2024-01-13 22:38:48 +0800

    WIP (Navbar) : styling in dropdown menu

--- src/components/NavbarComponents.jsx
+++ src/components/NavbarComponents.jsx
@@ -93,18 +93,19 @@ function Navbar() {
             </div>
             {isProfileOpen && (
               <div
-                className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5"
+                className="origin-top-right absolute right-0 mt-2 w-56 rounded-2xl shadow-lg bg-white ring-1 ring-black ring-opacity-5"
                 role="menu"
                 aria-orientation="vertical"
                 aria-labelledby="avatar-dropdown"
               >
                 <div className="py-1" role="none">
                   {/* Dropdown items */}
-                  <div className="flex justify-center items-center border-b border-hijau mb-2">
-                    Halo, {userName} 👋
+                  <div className="flex flex-col justify-start items-start border-b border-hijau mb-2 p-2">
+                    <span className="text-lg font-semibold">{userName}</span>
+                    <span className="text-sm">geraldi@gmail.com</span>
                   </div>
 
-                  <div className="block px-4 py-2 text-sm text-black-600">
+                  <div className="block text-sm text-black-600">
                     <a
                       href="/profile"
                       className="block px-4 py-2 text-sm text-black-600 hover:bg-green-100 hover:text-hijau"

098a7410dc86c9c1a7ce15b48bf2793aa7abd70f
GeraldiNathan <geraldi.tommysaputra03@gmail.com>
2024-01-17 19:40:40 +0800

    feat(TeamPage) : create new page for team page

---
+++ src/pages/OurTeamPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import OurTeamComponent from "../components/OurTeamComponent";
+
+function OurTeamPage() {
+  return (
+    <>
+      <OurTeamComponent />
+    </>
+  );
+}
+
+export default OurTeamPage;
